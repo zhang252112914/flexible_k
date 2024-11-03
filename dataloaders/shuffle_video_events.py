fb27660edd77e33c84a3e7a834c3755dba3e9a0f
Roy <zyl2010286495@gmail.com>
2024-11-03 21:42:11 +0800

    11.3.4

--- dataloaders/shuffle_video_events.py
+++ dataloaders/shuffle_video_events.py
@@ -18,6 +18,6 @@ def shuffle_video_events(segment_num, pairs, video, video_mask, duration):
         end_time = pair[1]
         start_frame = int(start_time / duration * frame_num)
         end_frame = int(end_time / duration * frame_num)
-        new_video[0][new_start:new_start + end_frame - start_frame] = video[0][start_frame:end_frame]
+        new_video[0][new_start:new_start + end_frame - start_frame, ...] = video[0][start_frame:end_frame, ...]
         new_start += (end_frame - start_frame)
     return new_video, video_mask
