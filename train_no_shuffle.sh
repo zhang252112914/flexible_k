1c99131fdd41b1621962aad071364c6037819aec
Roy <2010286495@qq.com>
2024-11-03 07:01:25 +0000

    11.3.1

--- train_no_shuffle.sh
+++ train_no_shuffle.sh
@@ -1,14 +1,14 @@
-CUDA_VISIBLE_DEVICES=0,2,3,5 torchrun --nproc_per_node=4 \
---master_port=53198 \
+CUDA_VISIBLE_DEVICES=0,2,5,6 torchrun --nproc_per_node=4 \
+--master_port=56198 \
 main.py --do_train \
 --num_thread_reader=2 \
 --data_path /home/zyl/MeVTR_data_and_models/charades/annotation \
 --features_path /home/zyl/MeVTR_data_and_models/charades/Charades_v2_3 \
---output_dir /home/zyl/flexible_k/output \
+--output_dir /home/zyl/flexible_k/output_no_shuffle \
 --max_words 77 --max_frames 64 --batch_size_val 16 \
 --datatype charades --feature_framerate 1 --coef_lr 1e-4 \
 --slice_framepos 2 \
 --loose_type --linear_patch 2d --sim_header meanP \
 --pretrained_clip_name ViT-B/32 \
 --post_process none --post_cluster_centroids 16 \
---batch_size 16
+--batch_size 8
