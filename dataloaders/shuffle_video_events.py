64be8a15ce69463903f11138dedfcfaee8fd9288
Roy <zyl2010286495@gmail.com>
2024-11-03 21:04:29 +0800

    11.3.4

--- dataloaders/shuffle_video_events.py
+++ dataloaders/shuffle_video_events.py
@@ -6,7 +6,7 @@ def shuffle_video_events(segment_num, pairs, video, video_mask, duration):
     shuffle_pairs.sort(key=lambda x: x[0])
     random.shuffle(shuffle_pairs)
     frame_num = np.sum(video_mask)
-    new_video = np.zeros(video.shape)
+    new_video = np.zeros_like(video.shape)
     new_start = 0
     for i in range(segment_num):
         pair = shuffle_pairs[i]
@@ -14,6 +14,6 @@ def shuffle_video_events(segment_num, pairs, video, video_mask, duration):
         end_time = pair[1]
         start_frame = int(start_time / duration * frame_num)
         end_frame = int(end_time / duration * frame_num)
-        new_video[new_start:new_start + end_frame - start_frame] = video[start_frame:end_frame]
+        new_video[0][new_start:new_start + end_frame - start_frame] = video[0][start_frame:end_frame]
         new_start += (end_frame - start_frame)
     return new_video, video_mask
