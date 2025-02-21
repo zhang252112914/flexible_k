117705e25292eaabc6170ec843d380b6f92bc5c8
Roy <2010286495@qq.com>
2025-02-21 03:50:31 +0000

    an acripts

--- script_an/train_topk.sh
+++ script_an/train_topk.sh
@@ -4,11 +4,11 @@ main.py --do_train \
 --num_thread_reader=4 \
 --data_path /home/zyl/activitynet/captions \
 --features_path /Data/datasets/ActivityNet/all_train_val_3fps \
---output_dir /home/zyl/flexible_k/output_an/output_base \
+--output_dir /home/zyl/flexible_k/output_an/output_topk \
 --max_words 77 --max_frames 64 --batch_size_val 16 \
 --datatype activity --feature_framerate 1 --coef_lr 1e-3 \
 --slice_framepos 2 \
 --loose_type --linear_patch 2d --sim_header meanP \
 --pretrained_clip_name ViT-B/32 \
---post_process cluster --post_cluster_centroids 16 \
+--post_process topk --post_cluster_centroids 16 \
 --batch_size 16
