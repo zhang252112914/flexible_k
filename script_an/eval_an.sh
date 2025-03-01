ad2d00fa665449de005c450e7b8a21b9739e8eec
yulin zhang <zyl@yzwgroup.com>
2025-03-01 11:21:13 +0000

    add an eval and resume

--- script_an/eval_an.sh
+++ script_an/eval_an.sh
@@ -1,10 +1,10 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 \
+CUDA_VISIBLE_DEVICES=2,5 torchrun --nproc_per_node=2 \
 --master_port=53198 \
 main.py --do_train \
 --num_thread_reader=1 \
 --data_path /home/zyl/activitynet/captions \
 --features_path /home/zyl/activitynet/all_train_val_3fps \
---output_dir /home/zyl/flexible_k/output_an/output_global_local5_new \
+--output_dir /home/zyl/flexible_k/output_an/output_global_local5_eval \
 --max_words 77 --max_frames 64 --batch_size_val 16 \
 --datatype activity --feature_framerate 1 --coef_lr 1e-3 \
 --slice_framepos 2 \
@@ -13,4 +13,5 @@ main.py --do_train \
 --post_process topk_event --post_cluster_centroids 16 \
 --batch_size 64 --local_contrastive \
 --global_attn --global_again --test_method all \
---local_weight 5.0 --K 32
+--local_weight 5.0 --K 32 \
+--init_model /home/zyl/flexible_k/output_an/output_global_local5/pytorch_model.bin.0
--- script_an/resume_train.sh
+++ script_an/resume_train.sh
@@ -4,7 +4,7 @@ main.py --do_train \
 --num_thread_reader=1 \
 --data_path /home/zyl/activitynet/captions \
 --features_path /home/zyl/activitynet/all_train_val_3fps \
---output_dir /home/zyl/flexible_k/output_an/output_global_local5_new \
+--output_dir /home/zyl/flexible_k/output_an/output_global_local5_resume \
 --max_words 77 --max_frames 64 --batch_size_val 16 \
 --datatype activity --feature_framerate 1 --coef_lr 1e-3 \
 --slice_framepos 2 \
@@ -13,4 +13,6 @@ main.py --do_train \
 --post_process topk_event --post_cluster_centroids 16 \
 --batch_size 64 --local_contrastive \
 --global_attn --global_again --test_method all \
---local_weight 5.0 --K 32
+--local_weight 5.0 --K 32 \
+--init_model /home/zyl/flexible_k/output_an/output_global_local5/pytorch_model.bin.0 \
+--resume_model /home/zyl/flexible_k/output_an/output_global_local5/pytorch_opt.bin.0
